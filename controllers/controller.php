24b9874bee9f4ff80f3aeec959c7215a71f6db08
Your Name <jamaltaibi@MacBook-Pro-de-Jamal.local>
2024-02-29 16:01:08 +0100

    Exo MVC

--- controllers/controller.php
+++ controllers/controller.php
@@ -1,23 +1,29 @@
 <?php
-    require_once "models/model.php";
-    class controller{
+    require_once "models/model.php";   
+
+    class controller{        
+        /** * Permet d'afficher le header et le debut du DocType html la base. */
         function index(){
             require_once "views/header.php";
             require_once "views/base.php";
-        }
+        }        
+        /** * Affiche le tableau des Taches*/
         function affichageTaches(){
             require_once 'views/tabaffichage.php';
-        }
+        }        
+        /** * Affiche le champs a remplir des Taches */
         function soumettreTache(){
             require "views/tabtache.php";
-        }
+        }        
+        /** * Fonction qui permet au controlleur d'acceder a la base de données afin d'Inserer la donnée. */
         function entrerTache(){
             if(isset($_POST['tache'])) {
                 $tache = htmlspecialchars($_POST['tache']);
                 $conn = new Bdd;
                 $conn->insertBdd($tache);
             } 
-        }
+        }        
+        /** * Fonction qui permet au controlleur d'acceder a la base de données afin de Modifier la donnée. */
         function modifierTache(){
             if(isset($_POST['idtache'], $_POST['nouvelleTache'])) {
                 $idTache = $_POST['idtache'];
@@ -25,7 +31,8 @@
                 $conn = new Bdd;
                 $conn->modifBdd($idTache, $nouvelleTache);
             } 
-        }
+        }        
+        /** * Fonction qui permet au controlleur d'acceder a la base de données afin de Supprimer la donnée. */
         function supptache(){
             $suppTache = $_POST["idTache"];
             $conn = new Bdd;
--- models/model.php
+++ models/model.php
@@ -1,9 +1,11 @@
 <?php 
-
+    
+    /** * Accés a la base de donnéés et au fonction de la  */
     class Bdd{
 
         public $connexion;
-
+                
+        /** * Permet d'acceder a la base de données */
         public function __construct() {
             $dsn = "mysql:host=localhost;dbname=test";
             $username = "root";
@@ -11,36 +13,48 @@
             $this->connexion = new PDO($dsn, $username, $password);
             $this->connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             // echo "vous êtes connecté à la bdd";
-        }
+        }        
+        /** * Permet de recuperer et lire la base de données choisie */
         function readAll(){
             $sql = "SELECT * FROM `Taches`";
             $request = $this->connexion->prepare($sql);
             $request->execute();
             return $request->fetchAll(PDO::FETCH_ASSOC);
-        }
+        }        
+        /**
+         * Permet d'inserer dans la base de données choisie
+         * @param $param1 $param1 [la tache a effectué] 
+         * */
         function insertBdd($param1){
             $sql = "INSERT INTO `Taches`(`tache`) VALUES (:tache)";
             $stmtinsert = $this->connexion->prepare($sql);
             $stmtinsert->bindParam(':tache', $param1);
             $stmtinsert->execute();
             echo "data bien ajouté à la bdd";
+        }        
+        /**
+         * Permet de Modifier dans la base de données choisie
+         * @param $idTache $idTache ['']
+         * @param $nouvelleTache $nouvelleTache [Modifie la tache enregistrer]
+         */
+        function modifBdd($idTache, $nouvelleTache){
+            $sql = "UPDATE `Taches` SET `tache` = :nouvelleTache WHERE `id` = :idTache";
+            $stmt = $this->connexion->prepare($sql);
+            $stmt->bindParam(':nouvelleTache', $nouvelleTache);
+            $stmt->bindParam(':idTache', $idTache);
+            $stmt->execute();
+        }        
+        /**
+         * Permet de Supprimer dans la base de données choisie
+         * @param $suppTache $suppTache [Supprime l'id et la tache]
+         */
+        function suppBdd($suppTache){
+            $sqlSup = "DELETE FROM `Taches` WHERE id = :idTache";
+            $stmtSup = $this->connexion->prepare($sqlSup);
+            $stmtSup->bindParam(':idTache', $suppTache);
+            $stmtSup->execute();
+
+            echo "data Supprimer";
         }
-
-    function modifBdd($idTache, $nouvelleTache){
-        $sql = "UPDATE `Taches` SET `tache` = :nouvelleTache WHERE `id` = :idTache";
-        $stmt = $this->connexion->prepare($sql);
-        $stmt->bindParam(':nouvelleTache', $nouvelleTache);
-        $stmt->bindParam(':idTache', $idTache);
-        $stmt->execute();
-    }
-
-    function suppBdd($suppTache){
-        $sqlSup = "DELETE FROM `Taches` WHERE id = :idTache";
-        $stmtSup = $this->connexion->prepare($sqlSup);
-        $stmtSup->bindParam(':idTache', $suppTache);
-        $stmtSup->execute();
-
-        echo "data Supprimer";
-    }
     }
 ?>
